9fff9f4dd3ac83d01537098b0996b223b9108f66
Jose Cardozo <cardoz.jg@gmail.com>
2023-08-22 18:45:36 -0400

    feat/button

--- WorkWave_client/src/App.tsx
+++ WorkWave_client/src/App.tsx
@@ -1,13 +1,11 @@
-
-import './App.css'
+import Buttons from "./components/Buttons";
+import "./App.css";
 function App() {
-
-
   return (
-    <div className='text-3xl font-bold underline bg-red-500'>
-     <h1 className='text-blue-600'>funcionando</h1>
+    <div className="text-3xl font-bold underline bg-red-500">
+      <h1 className="text-blue-600">funcionando</h1>
     </div>
-  )
-} 
+  );
+}
 
-export default App
+export default App;
