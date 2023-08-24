8e07ef69017f07db9ab7c4707908ebf43509d57f
Jose Cardozo <cardoz.jg@gmail.com>
2023-08-24 08:39:50 -0400

    Feat/Stepper

--- WorkWave_client/src/components/Buttons.tsx
+++ WorkWave_client/src/components/Buttons.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 type ButtonProps = {
   text: string; // texto que aparecera en el boton
   blue: boolean; // cambio de estilos del boton si es azul o transparente
@@ -7,7 +8,7 @@ type ButtonProps = {
 function Buttons(props: ButtonProps) {
   const { text = "", routing = "/", blue = true } = props;
   return (
-    <a href={routing}>
+    <Link to={routing}>
       <button
         className={`h-11 w-[300px] sm:w-[450px]   rounded-lg p-1 text-center border-2  ${
           blue ? "bg-[#61A9FD] text-[#000C2C]" : "bg-inherit text-[#D9D9D9]"
@@ -15,7 +16,7 @@ function Buttons(props: ButtonProps) {
       >
         {text}
       </button>
-    </a>
+    </Link>
   );
 }
 
