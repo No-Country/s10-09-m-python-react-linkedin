df39cf94b79fd97fce69b9ff61592deab2007d98
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2023-09-07 19:14:22 -0300

    the register was conected to  the  backend

--- WorkWave_client/src/pages/Profile/Profile.tsx
+++ WorkWave_client/src/pages/Profile/Profile.tsx
@@ -6,14 +6,14 @@ import { AiOutlineMail } from "react-icons/ai";
 import { FaPhoneAlt } from "react-icons/fa";
 import { AiFillBehanceSquare } from "react-icons/ai";
 import NavbarUserProfile from "../../components/NavbarUserProfile/NavbarUserProfile";
+
 const Profile: React.FC = () => {
   return (
-    <div className="container  mx-auto">
-      {/* ACA EMPIEZA EL BANNER DEL USUARIO */}
-
-      <div className="flex flex-col md:flex-row w-[1116.81px] h-[473px] mx-auto bg-black">
+    <div className="container mx-auto">
+      {/* BANNER DEL USUARIO */}
+      <div className="w-full md:flex md:w-2/3 mx-auto bg-black">
         <div className="w-full">
-          <div className="bannerProfileContainer ">
+          <div className="bannerProfileContainer">
             <div className="bannerProfileUser">
               <img
                 src={banner}
@@ -21,26 +21,26 @@ const Profile: React.FC = () => {
                 className="w-full rounded-sm"
               />
             </div>
-            <div className="dataUser">
-              <div>
-                <div>
-                  <img
-                    src={UserImg}
-                    alt=""
-                    className="userImg border border-white rounded-full p-2 my-2 mx-6"
-                  />
+            <div className="dataUser mt-5 md:flex md:justify-between md:items-center">
+              <div className="md:flex md:items-center">
+                <img
+                  src={UserImg}
+                  alt=""
+                  className="userImg border border-white rounded-full p-2 my-2 md:mx-6"
+                />
+                <div className="md:text-center">
+                  <h1 className="text-xl md:text-2xl">UserName</h1>
+                  <p className="text-sm md:text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    dapibus eros eu vehicula interdum.
+                  </p>
                 </div>
               </div>
-              <div className="mt-5">
-                <h1 className="ml-2">UserName</h1>
-                <p className="ml-2">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  dapibus eros eu vehicula interdum.
-                </p>
-              </div>
-              <div className="flex justify-around mt-3">
-                <div className="rounded-xl bg-blue-500 p-1">300 CONEXIONES</div>
-                <div className="rounded-xl bg-transparent border-solid border-2 px-10">
+              <div className="flex flex-col md:flex-row md:mt-3">
+                <div className="rounded-xl bg-blue-500 p-1 md:mr-2 md:mb-0 mb-2">
+                  300 CONEXIONES
+                </div>
+                <div className="rounded-xl bg-transparent border-solid border-2 px-4">
                   EMPLEADO
                 </div>
               </div>
@@ -48,14 +48,13 @@ const Profile: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="flex gap-2 p-2 justify-center">
-        {/* ACA EMPIEZA LA INFORMACIO DEL USUARIO */}
-        {/* <div className=" aboutTheUserContainer   "> */}
-        <div className="flex flex-col aboutTheUserData md:w-[40%] sm:w-full">
-          <div className=" mt-3 p-3 bg-black rounded-md mb-3">
-            <h2>Sobre mí</h2>
+      <div className="flex flex-col md:flex-row gap-2 p-2 justify-center">
+        {/* INFORMACIÓN DEL USUARIO */}
+        <div className="flex flex-col aboutTheUserData md:w-1/2 sm:w-full mt-3">
+          <div className="p-3 bg-black rounded-md mb-3">
+            <h2 className="text-xl">Sobre mí</h2>
             <div className="UserInfo">
-              <p>
+              <p className="text-sm md:text-base">
                 Curabitur rutrum, diam id consequat consequat, nibh odio
                 venenatis sapien, a porta arcu orci a diam. Quisque et est
                 interdum, accumsan purus vitae, cursus nisl.
@@ -63,8 +62,8 @@ const Profile: React.FC = () => {
               </p>
             </div>
           </div>
-          <div className="aboutTheUserContact  p-3 bg-black rounded-md">
-            <h2 className="border-b-2 border-gray-500">CONTACTO</h2>
+          <div className="aboutTheUserContact p-3 bg-black rounded-md">
+            <h2 className="border-b-2 border-gray-500 text-xl">CONTACTO</h2>
             <div className="m-1 flex gap-1">
               <h2>
                 <AiOutlineMail className="mt-1 text-xl" />
@@ -73,24 +72,21 @@ const Profile: React.FC = () => {
             </div>
             <div className="m-1 flex gap-1">
               <h2>
-                <FaPhoneAlt className="mt-1 flex gap-1" />{" "}
+                <FaPhoneAlt className="mt-1 flex gap-1" />
               </h2>
               <p>+549114777777</p>
             </div>
             <div className="m-1 flex gap-1">
               <h2>
-                <AiFillBehanceSquare className="mt-1 text-xl" />{" "}
+                <AiFillBehanceSquare className="mt-1 text-xl" />
               </h2>
               <p>Usuario 1</p>
             </div>
           </div>
         </div>
-        {/* </div> */}
         {/* PROYECTOS Y EXPERIENCIA DEL USUARIO */}
-
-        <div className="UserProjectsContainer flex flex-col sm:w-[52%]  mt-3 p-2  ">
+        <div className="UserProjectsContainer sm:w-full md:w-1/2 mt-3 p-2">
           <NavbarUserProfile />
-          {/* <UserProjects/> */}
         </div>
       </div>
     </div>
