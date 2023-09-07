d9aa625782d0635534791cf1f66f0f26697a3e43
Elias Velazquez <evelazquez2604@gmail.com>
2023-09-06 21:50:34 -0300

    chore(settings): Update wsgi config

--- workwave_server/workwave/config/wsgi.py
+++ workwave_server/workwave/config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workwave.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workwave.config.base')
 
 application = get_wsgi_application()
