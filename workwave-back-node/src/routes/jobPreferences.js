ecb37c90213f5cde26215b01c5f476d7dfde2c6f
Adhemir Sabino <adhemirsabino@gmail.com>
2023-09-07 16:07:43 -0500

    CRUD and documentation job Applications and job Preferences

--- workwave-back-node/src/routes/jobPreferences.js
+++ workwave-back-node/src/routes/jobPreferences.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 const {
   createJobPreferences,
+  getJobPreferencesByUser,
+  deleteJobPreferenceByUserAndJob,
 } = require("../controllers/jobPreferences.controllers");
 
+router.get("/:userId", getJobPreferencesByUser);
 router.post("/", createJobPreferences);
+router.delete("/:userId/:jobId", deleteJobPreferenceByUserAndJob);
 module.exports = router;
