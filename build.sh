81d46ad5c39d8a9bd92ea45efd9d656a3ab88563
Elias Velazquez <evelazquez2604@gmail.com>
2023-09-11 15:55:42 -0300

    Update build file for production

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ pipenv install
 
 pipenv run pip install -r workwave_server/requirements.txt
 
-python ./workwave_server/manage.py collectstatic --no-input --settings=workwave.config.render
-python ./workwave_server/manage.py migrate --settings=workwave.config.render
+python ./workwave_server/manage.py collectstatic --no-input --settings=workwave.config.prod
+python ./workwave_server/manage.py migrate --settings=workwave.config.prod
